696f085db9cd6e4f110b9678fb28d96c95f2f81a
Juha Wilppu <juha.wilppu@gmail.com>
2019-03-22 07:04:12 +0200

    Fix typo in .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.google.clientId,
+            clientSecret: keys.google.clientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken) => console.log(accessToken)
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });
