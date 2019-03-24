b5f43fa59298f1c630f21c931a9f8efca027a7b1
Juha Wilppu <juha.wilppu@gmail.com>
2019-03-24 18:53:03 +0200

    Part 1/2 works

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const axios = require('axios');
 
 require('./models/User');
 require('./models/Message');
@@ -40,3 +41,38 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 console.log('Server started')
+
+console.log('making call to graphql');
+
+//const from = "Adjutantinkatu 3, 02650 ESPOO";
+//const to = "Vaisalantie 6, ESPOO";
+
+axios({
+    url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+    method: 'post',
+    data: {
+        query: `{
+            plan(
+              from: {lat: 60.168992, lon: 24.932366}
+              to: {lat: 60.175294, lon: 24.684855}
+              numItineraries: 1
+            ) {
+              itineraries {
+                legs {
+                  duration
+                  distance
+                }
+              }
+            }
+        }`
+    }
+}).then(response => {
+    console.log(response.data);
+
+    const duration = response.data.data.plan.itineraries[0].legs.map(leg => leg.duration).reduce((a, b) => a+b, 0) / 60;
+    const distance = response.data.data.plan.itineraries[0].legs.map(leg => leg.distance).reduce((a, b) => a+b, 0) / 1000;
+
+    console.log(`total duration ${duration.toFixed(0)} min`)
+    console.log(`total distance ${distance.toFixed(1)} km`)
+
+});
